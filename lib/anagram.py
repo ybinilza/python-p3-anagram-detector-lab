cb86e3a7dc1080f95fb19f3ac08e6a0dbee69241
Binil Thomas Zachariah <binilreni7@gmail.com>
2023-08-12 00:26:52 -0400

    Lab completed

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,14 @@
-# your code goes here!
+class Anagram:
+    def __init__(self,word):
+        self.word=word;
+    
+    def match(self,word_list):
+        a=[];
+        print(a)
+        for item in word_list:
+            if sorted(item) == sorted(self.word):
+                a.append(item);
+        return(a)
+
+listen = Anagram("listen")
+print(listen.match(['enlists', 'google', 'inlets', 'banana']))
